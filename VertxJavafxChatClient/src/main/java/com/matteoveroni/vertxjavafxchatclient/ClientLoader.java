b2b0abca37374c16ade7a3b60b851cc7e985bdfd
Matteo Veroni <matver87@gmail.com>
2017-09-11 12:05:03 +0200

    added VertxJavafxChatServer - VertxJavafxChatClient - VertxJavafxChatBusinessLogic gradle subprojects

---
+++ VertxJavafxChatClient/src/main/java/com/matteoveroni/vertxjavafxchatclient/ClientLoader.java
@@ -0,0 +1,9 @@
+package com.matteoveroni.vertxjavafxchatclient;
+
+public class ClientLoader {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
---
+++ VertxJavafxChatServer/src/main/java/com/matteoveroni/vertxjavafxchatserver/ServerLoader.java
@@ -0,0 +1,9 @@
+package com.matteoveroni.vertxjavafxchatserver;
+
+public class ServerLoader {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+}
